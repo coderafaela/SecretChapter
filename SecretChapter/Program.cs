afe50c2d9e260bc3896cd968eb5bb363c34a6e9f
coderafaela <aquevedorafaela@gmail.com>
2026-07-09 02:34:16 -0300

    #Refatora domínio e implementa menu interativo
    Refatora as classes Autor, Livro e Saga, organizando-as no namespace SecretChapter.Modelos e simplificando suas responsabilidades. Implementa um menu interativo em Program.cs para registrar autores, livros, avaliações e exibir informações, tornando o código mais modular e orientado a dados. Remove métodos de exibição e exemplos fixos.

--- SecretChapter/Program.cs
+++ SecretChapter/Program.cs
@@ -1,37 +1,222 @@
-﻿Autor robertJacksonBennett = new Autor("Robert Jackson Bennett");
+﻿using SecretChapter.Modelos;
 
-Saga shadowoftheLevithan = new Saga("Shadow of the Leviathan");
+Autor claireN = new Autor("Claire North");
 
-Livro livro1 = new Livro(robertJacksonBennett, "O Cálice Contaminado")
+Autor joeA = new Autor("Joe Abercrombie");
+
+Dictionary<string, Autor> autoresRegistrados = new();
+
+
+void ExibirLogo()
 {
-    Paginas = 448,
-    Genero = "Fantasia/Ficção Científica LGBTQ+",
-    Traducao = true,
-    Sinopse = "Em uma opulenta mansão de Daretana, um oficial imperial de alto escalão morre de repente após uma planta enorme florescer de seu corpo. Completamente fora do comum, todos os indícios do caso apontam para um assassinato, até então sem pista alguma. Agora, a população local corre um grande risco ― mesmo ali, às margens do império, onde existem preocupações maiores por causa dos temíveis leviatãs que invadem as terras de tempos em tempos.\r\n\r\nSó uma detetive de carreira respeitável poderia resolver o caso. Entram em cena Ana Dolabra, de mente afiada e atitudes excêntricas, capaz de solucionar os crimes mais diversos sem nem mesmo sair de casa, e seu novo assistente Dinios Kol, dono de uma habilidade de memorização muito conveniente para uma investigação. No entanto, ambos guardam os próprios enigmas à medida que desvendam uma rede que põe em xeque o próprio Império, caso a misteriosa planta não seja contida.",
-};
+    Console.WriteLine(@"
 
+░██████╗███████╗░█████╗░██████╗░███████╗████████╗  ░█████╗░██╗░░██╗░█████╗░██████╗░████████╗███████╗██████╗░
+██╔════╝██╔════╝██╔══██╗██╔══██╗██╔════╝╚══██╔══╝  ██╔══██╗██║░░██║██╔══██╗██╔══██╗╚══██╔══╝██╔════╝██╔══██╗
+╚█████╗░█████╗░░██║░░╚═╝██████╔╝█████╗░░░░░██║░░░  ██║░░╚═╝███████║███████║██████╔╝░░░██║░░░█████╗░░██████╔╝
+░╚═══██╗██╔══╝░░██║░░██╗██╔══██╗██╔══╝░░░░░██║░░░  ██║░░██╗██╔══██║██╔══██║██╔═══╝░░░░██║░░░██╔══╝░░██╔══██╗
+██████╔╝███████╗╚█████╔╝██║░░██║███████╗░░░██║░░░  ╚█████╔╝██║░░██║██║░░██║██║░░░░░░░░██║░░░███████╗██║░░██║
+╚═════╝░╚══════╝░╚════╝░╚═╝░░╚═╝╚══════╝░░░╚═╝░░░  ░╚════╝░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░░░░░░░╚═╝░░░╚══════╝╚═╝░░╚═╝");
 
-Livro livro2 = new Livro(robertJacksonBennett, "A Drop of Corruption")
+    Console.WriteLine(@"
+
+█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█
+█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█
+█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█
+");
+    Console.WriteLine("Bem-vinde ao Secret Chapter");
+}
+void ExibirOpcoesDoMenu()
 {
-    Genero = "Fantasia/Ficção Científica LGBTQ+",
-    Traducao = false,
-};
+    ExibirLogo();
+    Console.WriteLine("\nDigite 1 para registrar um autor");
+    Console.WriteLine("Digite 2 para registrar o livro de um autor");
+    Console.WriteLine("Digite 3 para mostrar todos os autores");
+    Console.WriteLine("Digite 4 para avaliar um livro");
+    Console.WriteLine("Digite 5 para exibir os detalhes de um autor");
+    Console.WriteLine("Digite -1 para sair");
 
-Livro livro3 = new Livro(robertJacksonBennett, "A Trade of Blood")
+    Console.Write("\nDigite a sua opção: ");
+    string opcaoEscolhida = Console.ReadLine()!;
+    int opcaoEscolhidaNum = int.Parse(opcaoEscolhida);
+    switch (opcaoEscolhidaNum)
+    {
+        case 1:
+            RegistrarAutor();
+            break;
+        case 2:
+            RegistrarLivros();
+            break;
+        case 3:
+            MostrarAutores();
+            break;
+        case 4:
+            AvaliarUmLivro();
+            break;
+        case 5:
+            ExibirInformacoes();
+            break;
+        case -1:
+            Console.WriteLine("Bye bye, bitch");
+            Environment.Exit(0);
+            break;
+        default:
+            Console.WriteLine("Opção Inválida");
+            break;
+    }
+
+}
+
+void RegistrarAutor()
 {
-    Genero = "Fantasia/Ficção Científica LGBTQ+",
-    Traducao = false,
-};
+    Console.Clear();
+    Console.WriteLine(@"
+
+█▀█ █▀▀ █▀▀ █ █▀ ▀█▀ █▀█ █▀█   █▀▄ █▀▀   ▄▀█ █░█ ▀█▀ █▀█ █▀█ █▀▀ █▀
+█▀▄ ██▄ █▄█ █ ▄█ ░█░ █▀▄ █▄█   █▄▀ ██▄   █▀█ █▄█ ░█░ █▄█ █▀▄ ██▄ ▄█");
+    Console.Clear();
+    Console.Write("Digite o nome do(a) autor(a) que deseja registrar: ");
+    string nomeDeAutor = Console.ReadLine()!;
+    if (!autoresRegistrados.ContainsKey(nomeDeAutor))
+    {
+        autoresRegistrados.Add(nomeDeAutor, new Autor(nomeDeAutor));
+    }
+    else
+    {
+        Console.WriteLine("Autor já registrado.");
+    }
+    Console.WriteLine($"O(A) autor(a) {nomeDeAutor} foi registrado(a) com sucesso!");
+    Console.WriteLine("\nDigite uma tecla para voltar ao menu: ");
+    Console.ReadKey();
+    Console.Clear();
+    ExibirOpcoesDoMenu();
+
+}
+
+void RegistrarLivros()
+{
+    Console.Clear();
+    Console.WriteLine(@"
+
+█▀█ █▀▀ █▀▀ █ █▀ ▀█▀ █▀█ █▀█   █▀▄ █▀▀   █░░ █ █░█ █▀█ █▀█ █▀
+█▀▄ ██▄ █▄█ █ ▄█ ░█░ █▀▄ █▄█   █▄▀ ██▄   █▄▄ █ ▀▄▀ █▀▄ █▄█ ▄█");
+    Console.WriteLine(" ");
+    Console.Write("Digite o autor cujo livro deseja registrar: ");
+    string nomeDeAutor = Console.ReadLine()!;
+    if (autoresRegistrados.ContainsKey(nomeDeAutor))
+    {
+        Console.Write("\nAgora digite o título do livro: ");
+        string tituloDoLivro = Console.ReadLine()!;
+        Autor autor = autoresRegistrados[nomeDeAutor];
+        autor.AdicionarLivro(new Livro(tituloDoLivro));
+        Console.WriteLine("\nLivro adicionado com sucesso!");
+    }
+    else
+    {
+        Console.WriteLine("Autor não registrado");
+
+    }
+    Console.WriteLine("\nDigite uma tecla para voltar ao menu: ");
+    Console.ReadKey();
+    Console.Clear();
+    ExibirOpcoesDoMenu();
+}
+
+void MostrarAutores()
+{
+    Console.Clear();
+    Console.WriteLine(@"
+▄▀█ █░█ ▀█▀ █▀█ █▀█ █▀▀ █▀
+█▀█ █▄█ ░█░ █▄█ █▀▄ ██▄ ▄█");
+    Console.WriteLine(" ");
+    foreach (string autor in autoresRegistrados.Keys)
+    {
+        Console.WriteLine(autor);
+    }
+    Console.WriteLine("\nDigite uma tecla para voltar ao menu: ");
+    Console.ReadKey();
+    Console.Clear();
+    ExibirOpcoesDoMenu();
 
 
-shadowoftheLevithan.AdicionarLivro(livro1);
-shadowoftheLevithan.AdicionarLivro(livro2);
-shadowoftheLevithan.AdicionarLivro(livro3);
-robertJacksonBennett.AdicionarSaga(shadowoftheLevithan);
+}
+
+void AvaliarUmLivro()
+{
+    Console.Clear();
+    Console.WriteLine(@"
+▄▀█ █░█ ▄▀█ █░░ █ █▀▀   █░█ █▀▄▀█   █░░ █ █░█ █▀█ █▀█
+█▀█ ▀▄▀ █▀█ █▄▄ █ ██▄   █▄█ █░▀░█   █▄▄ █ ▀▄▀ █▀▄ █▄█");
+    Console.WriteLine(" ");
+    Console.Write("Digite o nome do livro que deseja avaliar: ");
+        string tituloDoLivro = Console.ReadLine()!;
+
+        Livro ? livroEncontrado = null;
+        foreach (var autor in autoresRegistrados.Values)
+        {
+            livroEncontrado = autor.Livros.FirstOrDefault(l => l.Titulo == tituloDoLivro);
+            if (livroEncontrado != null) break;
+        }
+
+        if (livroEncontrado != null)
+        {
+            Console.Write($"Digite o número de estrelas para {tituloDoLivro}: ");
+            double estrelasLivro = double.Parse(Console.ReadLine()!);
+            livroEncontrado.AdicionarEstrela(estrelasLivro);
+            Console.WriteLine($"\nAs {estrelasLivro} estrelas foram registradas com sucesso para o livro {tituloDoLivro}!");
+        }
+        else
+        {
+            Console.WriteLine($"\nOps, o livro {tituloDoLivro} não foi encontrado.");
+        }
+
+        Console.WriteLine("\nDigite uma tecla para voltar ao menu: ");
+        Console.ReadKey();
+        Console.Clear();
+        ExibirOpcoesDoMenu();
+ }
 
 
-robertJacksonBennett.ExibirAutor();
-shadowoftheLevithan.ExibirSaga();
+void ExibirInformacoes()
+{
+    Console.Clear();
+    Console.WriteLine(@"
+
+
+█ █▄░█ █▀▀ █▀█ █▀█ █▀▄▀█ ▄▀█ █▀▀ █▀█ █▀▀ █▀   █▀ █▀█ █▄▄ █▀█ █▀▀   █▀█ ▄▀ ▄▀█ ▀▄   ▄▀█ █░█ ▀█▀ █▀█ █▀█ ▄▀ ▄▀█ ▀▄
+█ █░▀█ █▀░ █▄█ █▀▄ █░▀░█ █▀█ █▄▄ █▄█ ██▄ ▄█   ▄█ █▄█ █▄█ █▀▄ ██▄   █▄█ ▀▄ █▀█ ▄▀   █▀█ █▄█ ░█░ █▄█ █▀▄ ▀▄ █▀█ ▄▀");
+    Console.WriteLine(" ");
+    Console.Write("Digite o nome do autor: ");
+    string nomeDeAutor = Console.ReadLine()!;
+
+    if (autoresRegistrados.ContainsKey(nomeDeAutor))
+    {
+        Autor autor = autoresRegistrados[nomeDeAutor];
+        Console.WriteLine($"\nAutor: {autor.Nome}");
+
+        if (autor.Livros.Count == 0)
+        {
+            Console.WriteLine("Nenhum livro registrado.");
+        }
+        else
+        {
+            foreach (var livro in autor.Livros)
+            {
+                Console.WriteLine($"- {livro.Titulo} | Estrelas: {livro.Media:F1}");
+            }
+        }
+    }
+    else
+    {
+        Console.WriteLine($"\nO(A) autor(a) {nomeDeAutor} não foi encontrado(a)!");
+    }
+
+    Console.WriteLine("Digite uma tecla para voltar ao menu: ");
+    Console.ReadKey();
+    Console.Clear();
+}
+    ExibirOpcoesDoMenu();
+
 
 
 
